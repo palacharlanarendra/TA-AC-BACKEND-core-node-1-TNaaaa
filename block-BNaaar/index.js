1533f6f5a3cccffb8bb5e1c80824d70fcdfd444a
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-06-23 11:44:05 +0530

    refactor the Handling multiple requests

--- block-BNaaar/index.js
+++ block-BNaaar/index.js
@@ -1,21 +1,22 @@
 let http = require('http');
 
-var server = http.createServer(handleRequest);
+let server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
   console.log(req.method);
+
   if (req.method === 'GET' && req.url === '/') {
-    res.write('Welcome to homepage');
-    res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Welcome to homepage');
   } else if (req.method === 'GET' && req.url === '/about') {
     res.setHeader('Content-Type', 'text/html');
     res.end('<h2>this is all about NodeJS</h2>');
   } else if (req.method === 'POST' && req.url === '/about') {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify('{message: this is a post request}'));
+    res.end('{"message": "this is a post request"}');
   }
 }
 
 server.listen(5000, () => {
-  console.log('server listening on port 5000');
+  console.log('listening to the port 5k');
 });
